11cf7463e9f3244b1db0b90b12f11ca532bdbdc2
Alexandre Lino <149592058+alexlinofer@users.noreply.github.com>
2024-10-18 10:41:24 -0300

    Added scale animation to the level pieces
    - LevelPieces agora fazem uma animação de aumento de escala quando é dado play na cena ou quando são criadas novas peças

--- Assets/Scripts/LevelManager/LevelManager.cs
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class LevelManager : MonoBehaviour
 {
@@ -17,6 +18,11 @@ public class LevelManager : MonoBehaviour
 
     [SerializeField] private List<LevelPieceBase> _spawnedPieces = new List<LevelPieceBase>();
     private LevelPieceBasedSetup _currSetup;
+
+    [Header("Animation")]
+    public float scaleDuration = .3f;
+    public float scaleTimeBetweenPieces = .2f;
+    public Ease ease = Ease.OutBounce;
 
     private void Awake()
     {
@@ -86,6 +92,24 @@ public class LevelManager : MonoBehaviour
 
         ColorManager.Instance.ChangeColorByType(_currSetup.artType);
 
+        StartCoroutine(ScalePiecesByTime());
+
+    }
+
+    IEnumerator ScalePiecesByTime()
+    {
+        foreach(var p in _spawnedPieces)
+        {
+            p.transform.localScale = Vector3.zero;
+        }
+
+        yield return null;
+
+        for(int i = 0; i <_spawnedPieces.Count; i++)
+        {
+            _spawnedPieces[i].transform.DOScale(1, scaleDuration).SetEase(ease);
+            yield return new WaitForSeconds(scaleTimeBetweenPieces);
+        }
     }
 
     private void CreateLevelPiece(List<LevelPieceBase> list)
